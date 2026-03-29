c7ab5680d7b6da8946e32d130f9522f4fb739108
Bahlaq <m.bahlaq94@gmail.com>
2026-03-29 10:15:24 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -12,14 +12,15 @@ const config: CapacitorConfig = {
       launchShowDuration: 2000,
       backgroundColor: "#4F46E5",
       showSpinner: false,
-      androidScaleType: "CENTER_CROP",
+      androidScaleType: "CENTER_INSIDE",
       splashFullScreen: true,
       splashImmersive: true,
     },
     GoogleAuth: {
       scopes: ["profile", "email"],
       serverClientId: "563861371307-3moj6n7qanfg0tgn1vrv8ok59rnh8pj2.apps.googleusercontent.com",
-      forceCodeForRefreshToken: true
+      forceCodeForRefreshToken: true,
+      redirectUri: "https://rewardhub-1ea27.firebaseapp.com/__/auth/handler"
     }
   }
 };
